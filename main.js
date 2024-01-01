787b36fb8de42c5e981e83b289fff1c12c795f91
angelAug10 <128250923+angelAug10@users.noreply.github.com>
2024-01-01 16:50:35 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+FontFamily:CurrentValue["FontFamily"],
+ Font-size=24,
+ FontColor=RGBColor[1,0,0],
+ FontWeight="Bold"
+ canvas=
+createCanvas(550,550);
+canvas.position(5,60,150);
+function draw()
+{
+background('#969A97');
+}
+function setup(){
+video=createCapture(VIDEO);
+video.size(550,500);
+
+canvas=createCanvas(550,550);
+canvas.position(560,150);
+}
+function draw(){
+background('#969A97');
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+console.log('PoseNet Is Intialized!');
+}
+function gotPoses(results)
+{
+if(results.length>0);
+{
+console.log(results);
+}
+}
